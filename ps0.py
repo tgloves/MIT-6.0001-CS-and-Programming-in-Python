989c170de06aa23dc6f33e54a5e795fafdfadba6
tgloves <45521720+tgloves@users.noreply.github.com>
2020-05-10 11:22:37 -0700

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 21:42:23 2020
+
+@author: terrence.glover
+"""
+import numpy as np
+
+x = int(input('Enter a number: '))
+y = int(input('Enter a number: '))
+print(x**y)
+print(np.log2(x))
